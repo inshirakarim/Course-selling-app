d36dd94884725099184391f2fe55787b89043051
Inshira Karim <karim.inshira@gmail.com>
2023-09-08 11:56:42 +0530

    Added files

---
+++ course-selling-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Signin from './Signin'
+import Signup from './Signup'
+
+function App() {
+
+  return (
+    <div>
+   <Signup></Signup>
+     {/* <Signin></Signin> */}
+     </div>
+    
+  )
+}
+
+export default App
---
+++ course-selling-app/src/Signup.jsx
@@ -0,0 +1,29 @@
+import './Signup.css';
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import {Card, Typography} from "@mui/material";
+
+
+
+function Signup() {
+return ( 
+<div>
+    <div className='heading'>
+<Typography variant={"h6"}>
+Welcome! Signup here.
+</Typography>
+</div>
+
+<div className='box'>
+ <Card className= "card" variant={"outlined"}>
+
+ <TextField fullWidth={true} id="outlined-basic" label="Email" variant="outlined" /> <br /><br />
+ <TextField fullWidth={true} id="outlined-basic" label="Password" variant="outlined" /><br /><br />
+ <Button size={"large"} variant="contained">Signup</Button>
+
+ </Card>
+ </div>
+    </div>
+)
+}
+export default Signup
